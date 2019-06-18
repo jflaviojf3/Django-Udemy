d590a1392825d8c5c5e5a58194717f7bcaf41a09
jflaviojf3 <jflaviojf3@hotmail.com>
2019-06-18 19:12:53 -0300

    Aula Crud Create

--- contas/views.py
+++ contas/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Transacao
 from django.http import HttpResponse
-
+from .form import TransacaoForm
 import datetime
 
 def home (request):
@@ -17,3 +17,15 @@ def listagem (request):
     data = {}
     data['transacoes'] = Transacao.objects.all()
     return render(request, 'contas/listagem.html', data)
+
+def nova_transacao(request):
+    data = {}
+    form = TransacaoForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('url_listagem')
+        
+    data['form'] = form
+
+    return render(request, 'contas/form.html', data)
